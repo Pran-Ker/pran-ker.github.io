7caea5ad8402cf40dc1bd1d9945b4d5ea3e5f768
Pran-Ker <hebbarpran@gmail.com>
2023-05-25 16:46:39 +0530

    Changes

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -67,7 +67,7 @@
 
 	// Typing Animation (Typed.js)
 	$('#element').typed({
-	    strings: ["UX, UI Designer", "Web App Developer", "Social Animal!"],
+	    strings: ["UX, UI Designer", "Web App Developer"],
 	    typeSpeed: -50,
 	    loop: true,
 	    startDelay: 500,
